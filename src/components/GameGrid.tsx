72b316124ee778c294bd454e06402a8314074359
Waleed Ali <waleed.ali@koderlabs.com>
2023-07-05 20:00:17 +0500

    Building Game Card

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,5 +1,13 @@
-import { Alert, AlertIcon, Button, useToast, Wrap } from "@chakra-ui/react";
+import {
+  Alert,
+  AlertIcon,
+  Button,
+  SimpleGrid,
+  useToast,
+  Wrap,
+} from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
+import GameCard from "./GameCard";
 
 const GameGrid = () => {
   const { games, error } = useGames();
@@ -8,11 +16,15 @@ const GameGrid = () => {
     <>
       {/* {error &&} */}
       <Wrap></Wrap>
-      <ul>
+      <SimpleGrid
+        padding="15px"
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        spacing={10}
+      >
         {games.map((game) => (
-          <li key={game.id}>{game.name}</li>
+          <GameCard key={game.id} game={game} />
         ))}
-      </ul>
+      </SimpleGrid>
     </>
   );
 };
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,9 +3,10 @@ import { CanceledError } from "axios";
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 
-interface Games {
+export interface Games {
   id: number;
   name: string;
+  background_image: string;
 }
 
 interface FetchGamesResponse {
